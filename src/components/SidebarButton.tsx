c0f2ca865290a6c72f49e743fa25938f79b31553
Gilberto Jr <giljrsantos@gmail.com>
2023-06-18 11:30:53 -0300

    format SideBarButton

--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -1,13 +1,20 @@
 import { ReactNode } from 'react';
 
 type Props = {
-    icon: ReactNode;
-    label: string;
-    onClick: () => void;
+  icon: ReactNode;
+  label: string;
+  onClick: () => void;
 };
 
-export const SidebarButton = ({ icon, label, onClick }: Props) => {
-    return (
-        <div onClick={onclick} className="flex items-center rounded-md p-3 text-sm cursor-pointer hover:bg-gray-500/20"></div>
-    );
+export const SidebarButton = ({
+  icon,
+  label,
+  onClick,
+}: Props) => {
+  return (
+    <div
+      onClick={onclick}
+      className="flex items-center rounded-md p-3 text-sm cursor-pointer hover:bg-gray-500/20"
+    ></div>
+  );
 };
